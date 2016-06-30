4fe76c01cd352c14b467900a770ccf104024689b
黄凯 <yujiayinshi@gmail.com>
2016-06-30 19:00:01 +0800

    图册

---
+++ script/datas.js
@@ -0,0 +1,35 @@
+/**
+ * Created by HuangKai on 2016/6/30.
+ */
+var users = [{
+    id: '1',
+    name: 'TanWan',
+    count: 10
+}, {
+    id: '2',
+    name: '多多',
+    count: 10
+}];
+
+var images = [{
+    userId: '1',
+    description: '价格实惠，好吃'
+},{
+    userId: '1',
+    description: '价格实惠，好吃'
+},{
+    userId: '1',
+    description: '价格实惠，好吃'
+},{
+    userId: '1',
+    description: '价格实惠，好吃'
+},{
+    userId: '1',
+    description: '价格实惠，好吃'
+},{
+    userId: '1',
+    description: '价格实惠，好吃'
+}, {
+    userId: '2',
+    description: '一般般，环境真好'
+}];
---
+++ script/index.js
@@ -0,0 +1,61 @@
+/**
+ * Created by HuangKai on 2016/6/30.
+ */
+$(function () {
+
+    init();
+
+    function init() {
+        renderRankList(users);
+        renderImages(images)
+    }
+
+    $('.item-link').click(function() {
+        localStorage.userId = $(this).attr('userId');
+        localStorage.userName = $(this).attr('userName');
+        renderImages(images);
+    });
+
+    function renderRankList(users) {
+        for (var i = 0, ii = users.length; i < ii; i++) {
+            var user = users[i];
+            var item =
+                '<li><a href="#images" userId="'+ user.id +'" userName="'+ user.name +'" class="item-link item-content">' +
+                '<div class="item-media"><i class="icon icon-f7"></i></div>' +
+                '<div class="item-inner">' +
+                '<div class="item-title">' + user.name + user.count +
+                '</div></div></a></li>';
+            $('.users-list').append(item);
+        }
+    }
+
+    function renderImages(images) {
+        $('.images-list').empty();
+        for (var i = 0, ii = images.length; i < ii; i++) {
+            var image = images[i];
+            var num = i + 1;
+            var url = 'data/' + localStorage.userId + '/' + num + '.png';
+            if (image.userId == localStorage.userId) {
+                var item =
+                    '<div class="card demo-card-header-pic">' +
+                    '<div valign="bottom" class="card-header color-white no-border no-padding">' +
+                    '<img class="card-cover" src="'+ url +'" alt="">' +
+                    '</div>' +
+                    '<div class="card-content">' +
+                    '<div class="card-content-inner">' +
+                        //'<p class="color-gray">发表于 2015/01/15</p>' +
+                    '<p>'+ image.description +'</p>' +
+                    '</div>' +
+                    '</div>' +
+                        //'<div class="card-footer">' +
+                        //'<a href="#" class="link">赞</a>' +
+                        //'<a href="#" class="link">更多</a>' +
+                        //'</div>' +
+                    '</div>';
+                $('.images-list').append(item);
+                $('.user-name').text(localStorage.userName);
+            }
+        }
+    }
+
+});
